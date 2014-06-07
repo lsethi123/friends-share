ff9c0eeb9a4f02e5a9b78665801688e75bba9bc0
lalitsethi <lalit.sethi@snapav.com>
2014-06-07 14:39:18 +0530

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsShare::Application.config.secret_token = '9c175d562d8ce026c791c9124892837e905c7fb33b95007852064e9ca0e3447e3990c2f8a2d8fceea5c784b495646287c70ae7aa4cae6a6258dbee32fd8510fc'
